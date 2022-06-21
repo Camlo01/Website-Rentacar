eed5be0c59a38c2eb0a4c91c6fe097e4be0360c3
camlo01 <camilobeltran174@gmail.com>
2022-06-21 11:49:33 -0500

    changed method that put data into the DOM

--- src/main/resources/static/js/Gama.js
+++ src/main/resources/static/js/Gama.js
@@ -2,30 +2,24 @@ function gamaData() {
   fetch("http://localhost:8080/api/Gama/all")
     .then((response) => response.json())
     .then(function (data) {
-      pintarRespuestaGama(data);
+      innerGamaData(data);
       addInSelect(data, "Select-Gama");
     })
     .catch((error) => console.log("Problema al mostrar la gama: " + error));
 }
 
-function pintarRespuestaGama(respuesta) {
-  let myTable = "<table>";
-  for (i = 0; i < respuesta.length; i++) {
-    myTable += "<tr>";
-    myTable += "<td>" + respuesta[i].name + "</td>";
-    myTable += "<td>" + respuesta[i].description + "</td>";
-    myTable +=
-      "<td> <button onclick=' actualizarGama(" +
-      respuesta[i].idGama +
-      ")'>Actualizar</button>";
-    myTable +=
-      "<td> <button onclick='borrarGama(" +
-      respuesta[i].idGama +
-      ")'>Borrar</button>";
-    myTable += "</tr>";
-  }
-  myTable += "</table>";
-  $("#resultadoGama").html(myTable);
+function innerGamaData(data) {
+  let myTable = `<table>`;
+  data.forEach((gama) => {
+    myTable += `<tr>`;
+    myTable += `<td> ${gama.name} </td>`;
+    myTable += `<td> ${gama.description} </td>`;
+    myTable += `<td><button onclick='updateGama(${gama.idGama})'>Actualizar</button></td>`;
+    myTable += `<td><button onclick="deleteGama(${gama.idGama})">Borrar</button></td>`;
+    myTable += `</tr>`;
+  });
+  myTable += `</table>`;
+  document.getElementById(`resultadoGama`).innerHTML = myTable;
 }
 
 function saveGama() {
