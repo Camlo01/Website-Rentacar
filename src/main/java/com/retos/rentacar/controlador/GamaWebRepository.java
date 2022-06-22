2d6ff11f8183ab1aae471dfa74be42c1861e1460
camlo01 <camilobeltran174@gmail.com>
2022-06-21 22:50:06 -0500

    Put Method mapped by @PutMapping('/update') implemented

--- src/main/java/com/retos/rentacar/controlador/GamaWebRepository.java
+++ src/main/java/com/retos/rentacar/controlador/GamaWebRepository.java
@@ -13,37 +13,47 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/Gama")
-@CrossOrigin(origins = "*", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 public class GamaWebRepository {
-    
+
     @Autowired
-    private GamaServices servicios;
+    private GamaServices serviciosGama;
+
     @GetMapping("/all")
-    public List <Gama> getGama(){
-        return servicios.getAll();
+    public List<Gama> getGama() {
+        return serviciosGama.getAll();
     }
+
     @GetMapping("/{id}")
     public Optional<Gama> getGama(@PathVariable("id") int idGama) {
-        return servicios.getGama(idGama);
+        return serviciosGama.getGama(idGama);
     }
 
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
     public Gama save(@RequestBody Gama gama) {
-        return servicios.save(gama);
+        return serviciosGama.save(gama);
     }
-    
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Gama update(@RequestBody Gama gama){
+        return serviciosGama.update(gama);
+    }
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public boolean delete(@PathVariable ("id") int id) {
-        return servicios.deleteGama(id);
+    public boolean delete(@PathVariable("id") int id) {
+        return serviciosGama.deleteGama(id);
     }
 }
