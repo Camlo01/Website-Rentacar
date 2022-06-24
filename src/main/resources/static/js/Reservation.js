232b60345f77b56fc9def9c371fd65c59201182b
camlo01 <camilobeltran174@gmail.com>
2022-06-24 16:41:09 -0500

    prepare new index

--- src/main/resources/static/js/Reservation.js
+++ src/main/resources/static/js/Reservation.js
@@ -2,36 +2,55 @@ function reservationData() {
   fetch("http://localhost:8080/api/Reservation/all")
     .then((response) => response.json())
     .then(function (data) {
-      pintarRespuestaReservation(data);
+      innerReservatonData(data);
     })
     .catch((error) => console.log("Problema al mostrar reservation: " + error));
 }
 
-
-function pintarRespuestaReservation(respuesta) {
-  let myTable = "<table>";
-  for (i = 0; i < respuesta.length; i++) {
-    myTable += "<tr>";
-    myTable += "<td>" + respuesta[i].startDate + "</td>";
-    myTable += "<td>" + respuesta[i].devolutionDate + "</td>";
-    myTable += "<td>" + respuesta[i].status + "</td>";
-    myTable += "<td>" + respuesta[i].client + "</td>";
-    myTable += "<td>" + respuesta[i].car + "</td>";
-    myTable +=
-      "<td> <button onclick=' actualizarReservation(" +
-      respuesta[i].idReservation +
-      ")'>Actualizar</button>";
-    myTable +=
-      "<td> <button onclick='borrarReservation(" +
-      respuesta[i].idReservation +
-      ")'>Borrar</button>";
-    myTable += "</tr>";
-  }
-  myTable += "</table>";
-  $("#resultadoReservation").html(myTable);
+function innerReservatonData(data) {
+  let myTable = `<table>`;
+  data.forEach((reser) => {
+    myTable += `<tr>`;
+    myTable += `<td><p>Cliente:</p>${reser.client.name} </td>`;
+    myTable += `<td><p>Reservó:</p>${reser.car.name} </td>`;
+    myTable += `<td><p>Inicia:</p>${reser.startDate} </td>`;
+    myTable += `<td><p>Termina:</p>${reser.devolutionDate} </td>`;
+    myTable += `<td><button onclick='updateReservation(${reser.idReservation})'> Actualizar</button></td>`;
+    myTable += `<td><button onclick='deleteReservation(${reser.idReservation})'> Borrar</button></td>`;
+    myTable += `</tr>`;
+  });
+  myTable += `</table>`;
+  document.getElementById(`resultadoReservation`).innerHTML = myTable;
 }
 
-function guardarReservation() {
+function saveCar() {
+  let startDate = document.getElementById("startDate").value;
+  let devolutionDate = document.getElementById("devolutionDate").value;
+  let status = document.getElementById("status").value;
+  let car = document.getElementById("Select-Car").value;
+  let client = document.getElementById("Select-Client").value;
+  let data = {
+    startDate: startDate,
+    devolutionDate: devolutionDate,
+    status: status,
+    car: car,
+    client: client,
+  };
+
+  fetch("http://localhost:8080/api/Reservation/save", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log("ERROR reservation" + error);
+    });
+}
+
+function guardarReservationDeprecated() {
   let var2 = {
     startDate: $("#startDate").val(),
     devolutionDate: $("#devolutionDate").val(),
