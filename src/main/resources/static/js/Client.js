736d74f6953000359e0717fe4d713426bc85a9ad
camlo01 <camilobeltran174@gmail.com>
2022-06-26 20:23:37 -0500

    UpdateClient() and DeleteClient() with ajax method changed to fetch

--- src/main/resources/static/js/Client.js
+++ src/main/resources/static/js/Client.js
@@ -16,14 +16,13 @@ function innerClientData(data) {
     myTable += `<td> ${client.name} </td>`;
     myTable += `<td> ${client.email} </td>`;
     myTable += `<td> ${client.password} </td>`;
-    myTable += `<td><button onclick='updateCar(${client.idGama})'> Actualizar</button></td>`;
-    myTable += `<td><button onclick='deleteCar(${client.idGama})'> Borrar</button></td>`;
+    myTable += `<td><button onclick='updateClient(${client.idClient})'> Actualizar</button></td>`;
+    myTable += `<td><button onclick='deleteClient(${client.idClient})'> Borrar</button></td>`;
     myTable += `</tr>`;
   });
   myTable += `</table>`;
   document.getElementById(`resultadoClient`).innerHTML = myTable;
 }
-
 
 function saveClient() {
   let email = document.getElementById("ClientEmail").value;
@@ -46,6 +45,7 @@ function saveClient() {
     .then(function (response) {
       if (response.status == 201) {
         console.log("Se creó el usuario correctamente");
+        clientData();
       }
     })
     .catch(function (error) {
@@ -53,48 +53,67 @@ function saveClient() {
     });
 }
 
-function updateClient(idElemento) {
-  let myData = {
-    idClient: idElemento,
-    email: $("#ClientEmail").val(),
-    password: $("#ClientPassword").val(),
-    name: $("#ClientName").val(),
-    age: $("#ClientAge").val(),
+function updateClient(idClient) {
+  let email = document.getElementById("ClientEmail").value;
+  let password = document.getElementById("ClientPassword").value;
+  let name = document.getElementById("ClientName").value;
+  let age = document.getElementById("ClientAge").value;
+
+  function noOneNull() {
+    if (
+      email.length == 0 ||
+      password.length == 0 ||
+      name.length == 0 ||
+      age.length == 0
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  let data = {
+    idClient: idClient,
+    email: email,
+    password: password,
+    name: name,
+    age: age,
   };
-  console.log(myData);
-  let dataToSend = JSON.stringify(myData);
-  $.ajax({
-    url: "http://localhost:8080/api/Client/update",
-    type: "PUT",
-    data: dataToSend,
-    contentType: "application/JSON",
-    datatype: "JSON",
-    success: function (_respuesta) {
-      $("#ClientEmail").val("");
-      $("#ClientPassword").val("");
-      $("#ClientName").val("");
-      $("#ClientAge").val("");
-      autoInicioClient();
-      alert("Se ha actualizado correctamente el client");
-    },
-  });
+  console.log(data);
+
+  if (noOneNull()) {
+    fetch("http://localhost:8080/api/Client/update", {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then(function (response) {
+        if (response.status == 201) {
+          console.log("Se actualizó el cliente");
+        }
+        clientData();
+      })
+      .catch(function (error) {
+        alert("error al actualizar el cliente " + error);
+      });
+  } else {
+    alert("Hay un elementro vacio");
+  }
 }
 
-function deleteClient(idElemento) {
-  let myData = {
-    id: idElemento,
-  };
-  let dataToSend = JSON.stringify(myData);
-  $.ajax({
-    url: "http://localhost:8080/api/Client/" + idElemento,
-    type: "DELETE",
-    data: dataToSend,
-    contentType: "application/JSON",
-    dataType: "JSON",
-    success: function (respuesta) {
-      $("#resultadoClient").empty();
-      autoInicioClient();
-      alert("Se ha borrado correctamente el client");
-    },
-  });
+function deleteClient(idClient) {
+  fetch("http://localhost:8080/api/Client/delete/" + idClient, {
+    method: "DELETE",
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then(function (response) {
+      if (response.status == 500) {
+        alert("No se pudo eliminar, verifica que no tengo un carro asociado");
+      }
+      if (response.status == 204) {
+        clientData();
+      }
+    })
+    .catch(function (error) {
+      console.log("El error fue: " + error);
+    });
 }
