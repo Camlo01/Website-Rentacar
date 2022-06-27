984d2b3591da595aef3dbb7d23d3516c6a7fe97a
camlo01 <camilobeltran174@gmail.com>
2022-06-26 20:20:42 -0500

    add put mapping

--- src/main/java/com/retos/rentacar/controlador/ClientWebRepository.java
+++ src/main/java/com/retos/rentacar/controlador/ClientWebRepository.java
@@ -13,27 +13,28 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/Client")
-@CrossOrigin(origins = "*", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+        RequestMethod.DELETE })
 public class ClientWebRepository {
-    @GetMapping("/holaMundoClient")
-    public String saludad(){
-    return "Hola Mundo Client";
-    }
-    
+
     @Autowired
     private ClientServices servicios;
+
     @GetMapping("/all")
-    public List <Client> getClient(){
+    public List<Client> getClient() {
         return servicios.getAll();
     }
+
     @GetMapping("/{id}")
     public Optional<Client> getClient(@PathVariable("idClient") int idClient) {
         return servicios.getClient(idClient);
@@ -45,11 +46,16 @@ public class ClientWebRepository {
         return servicios.save(client);
     }
 
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client update(@RequestBody Client client) {
+        return servicios.update(client);
+    }
+
     @DeleteMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public boolean delete(@PathVariable ("id") int idClient) {
+    public boolean delete(@PathVariable("id") int idClient) {
         return servicios.deleteClient(idClient);
     }
-    
-    
+
 }
