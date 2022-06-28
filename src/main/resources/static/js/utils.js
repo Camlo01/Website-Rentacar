1957968169daf6d1b07120540836bd57db5db9e5
camlo01 <camilobeltran174@gmail.com>
2022-06-27 22:27:41 -0500

    implement function for every data to insert into it select and comment what function implement in th future

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -1,6 +1,47 @@
-function addInSelect(data, selectName) {
+/**
+ *
+ * @param {dates getted from database} data
+ * @param {where to inject select} selectName
+ */
+function addInSelectClient(data, selectName) {
   let selector = document.getElementById(selectName);
+
   for (let i = 0; i < data.length; i++) {
-    selector.options[i] = new Option(data[i].name);
+    let opcion = `${data[i].idClient} - ${data[i].name}`;
+    selector.options[i] = new Option(opcion);
   }
 }
+
+/**
+ *
+ * @param {dates getted from database} data
+ * @param {where to inject select} selectName
+ */
+function addInSelectCar(data, selectName) {
+  let selector = document.getElementById(selectName);
+
+  for (let i = 0; i < data.length; i++) {
+    let opcion = `${data[i].idCar} - ${data[i].name}`;
+    selector.options[i] = new Option(opcion);
+  }
+}
+
+/**
+ *
+ * @param {dates getted from database} data
+ * @param {where to inject select} selectName
+ */
+function addInSelectGama(data, selectName) {
+  let selector = document.getElementById(selectName);
+
+  for (let i = 0; i < data.length; i++) {
+    let opcion = `${data[i].idGama} - ${data[i].name}`;
+    selector.options[i] = new Option(opcion);
+  }
+}
+
+//Crear funcion que reciba un array y verifique que todos sus elementos para los input estén ocupado y no estén vacíos
+
+// Crear una funcion que separe el id del select con el método split()
+
+// Crear función que busca el elemento por su nombre para concatenarlo dentro de la creación del elemento
