cf885313a51f1496d07ef9f79608fa7799fc3406
camlo01 <camilobeltran174@gmail.com>
2022-06-29 16:52:41 -0500

    cleaned code

--- src/main/java/com/retos/rentacar/repositorio/ClientRepository.java
+++ src/main/java/com/retos/rentacar/repositorio/ClientRepository.java
@@ -7,22 +7,24 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public class ClientRepository {
     @Autowired
     private ClientInterface crudClient;
-    
-    public List<Client> getAll(){
+
+    public List<Client> getAll() {
         return (List<Client>) crudClient.findAll();
     }
-    public Optional <Client> getClient(int id){
+
+    public Optional<Client> getClient(int id) {
         return crudClient.findById(id);
     }
-        public Client save(Client client){
+
+    public Client save(Client client) {
         return crudClient.save(client);
     }
-        public void delete(Client client){
+
+    public void delete(Client client) {
         crudClient.delete(client);
     }
 }
