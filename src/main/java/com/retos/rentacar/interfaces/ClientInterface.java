94d70762751731bd5f7b78563beb6acbc2003e08
camlo01 <camilobeltran174@gmail.com>
2022-06-30 17:13:05 -0500

    implemented method that return Client by email

--- src/main/java/com/retos/rentacar/interfaces/ClientInterface.java
+++ src/main/java/com/retos/rentacar/interfaces/ClientInterface.java
@@ -5,12 +5,17 @@
 package com.retos.rentacar.interfaces;
 
 import com.retos.rentacar.modelo.Client;
+
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 /**
  *
  * @author USUARIO
  */
-public interface ClientInterface extends CrudRepository<Client,Integer> {
-    
+public interface ClientInterface extends CrudRepository<Client, Integer> {
+
+    // @Query("from Client ")
+    Optional<Client> findClientByEmail(String email);
 }
