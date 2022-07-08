b710e310bd6e0b6d20fbbfaa810de6fec5ce775c
camlo01 <camilobeltran174@gmail.com>
2022-07-08 12:16:17 -0500

    cleaned code

--- src/main/java/com/retos/rentacar/controlador/ClientWebRepository.java
+++ src/main/java/com/retos/rentacar/controlador/ClientWebRepository.java
@@ -3,8 +3,10 @@ package com.retos.rentacar.controlador;
 
 import com.retos.rentacar.modelo.Client;
 import com.retos.rentacar.servicios.ClientServices;
+
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
@@ -25,8 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/Client")
-@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
-        RequestMethod.DELETE })
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+        RequestMethod.DELETE})
 public class ClientWebRepository {
 
     @Autowired
