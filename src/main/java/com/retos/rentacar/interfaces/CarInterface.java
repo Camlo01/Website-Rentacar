ff07b9448fe839ccd2aa092580dac0082e180df8
camlo01 <camilobeltran174@gmail.com>
2022-07-10 16:47:58 -0500

    added pageable with interface PagingAndSortingRepository

--- src/main/java/com/retos/rentacar/interfaces/CarInterface.java
+++ src/main/java/com/retos/rentacar/interfaces/CarInterface.java
@@ -11,12 +11,13 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  *
  * @author USUARIO
  */
-public interface CarInterface extends CrudRepository<Car, Integer> {
+public interface CarInterface extends PagingAndSortingRepository<Car, Integer> {
 
     @Query(value = "SELECT * FROM car ORDER BY id_car DESC LIMIT 1",
     nativeQuery= true)
