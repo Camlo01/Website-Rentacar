66b1c75308e189764b8920843ef7b50e489932f0
camlo01 <camilobeltran174@gmail.com>
2022-07-15 23:13:41 -0500

    added request for cars pageeable and save car with validation of KeyClient

--- src/main/java/com/retos/rentacar/controlador/CarWebRepository.java
+++ src/main/java/com/retos/rentacar/controlador/CarWebRepository.java
@@ -2,8 +2,11 @@
 package com.retos.rentacar.controlador;
 
 import com.retos.rentacar.interfaces.CarInterface;
+import com.retos.rentacar.interfaces.ClientInterface;
 import com.retos.rentacar.modelo.Car;
 import com.retos.rentacar.modelo.CarStatus;
+import com.retos.rentacar.modelo.Client;
+import com.retos.rentacar.modelo.KeyClient;
 import com.retos.rentacar.servicios.CarServices;
 
 import java.util.List;
@@ -13,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +30,33 @@ public class CarWebRepository {
     private CarServices carServices;
     @Autowired
     private CarInterface carInterface;
+    @Autowired
+    private ClientInterface awd;
+
+    //--- Peticiones HTTP Fijas
+
+    //- GET
+    @GetMapping("/home_cars/size={size}page={page}")
+    List<Car> getListCarsToHomePage(@PathVariable("size") int size, @PathVariable("page") int page) {
+        return (List<Car>) carInterface.getCarsWithStatusBookable(size, page);
+    }
+
+    @GetMapping("/lastCarAdded")
+    public Optional<Car> getLastCarAdded() {
+        return carServices.getLastCarAdded();
+    }
+
+    //- POST
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Car saveVehicleWithAutoritation(@RequestBody Car car,
+                                           @RequestBody KeyClient keyClient) {
+        return carServices.saveVehicle(car, keyClient);
+    }
+
+
+    //--- Peticiones HTTP de recurso
 
     @GetMapping("/all")
     public List<Car> getCar() {
@@ -37,20 +68,9 @@ public class CarWebRepository {
         return carServices.getCar(id);
     }
 
-
-    @GetMapping("/home_cars/size={size}page={page}")
-    List<Car> getListCarsToHomePage(@PathVariable("size") int size, @PathVariable("page") int page) {
-        return (List<Car>) carInterface.getCarsWithStatusBookable(size, page);
-    }
-
     @GetMapping("/pageable")
     List<Car> getCarPageable(@RequestParam int page, @RequestParam int size) {
         return carInterface.findAll(PageRequest.of(page, size)).getContent();
-    }
-
-    @GetMapping("/lastCarAdded")
-    public Optional<Car> getLastCarAdded() {
-        return carServices.getLastCarAdded();
     }
 
     @PostMapping("/save")
