dc4b2a0e082baee270a7c761696b8a1111122a2f
camlo01 <camilobeltran174@gmail.com>
2022-07-15 23:00:38 -0500

    added class to define type of client

---
+++ src/main/java/com/retos/rentacar/modelo/ClientType.java
@@ -0,0 +1,8 @@
+package com.retos.rentacar.modelo;
+
+public enum ClientType {
+        CLIENT,
+        ADMIN,
+        SUPPORT,
+        DEVELOPER
+}
---
+++ src/main/java/com/retos/rentacar/modelo/KeyClient.java
@@ -0,0 +1,2 @@
+package com.retos.rentacar.modelo;public class KeyClient {
+}
