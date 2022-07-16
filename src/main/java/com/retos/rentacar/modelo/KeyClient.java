2e153f7546b39ac245caf57e46262800d5e56657
camlo01 <camilobeltran174@gmail.com>
2022-07-15 23:04:23 -0500

    added KeyClient Object with constructor | getter | generateKeyClient() and hasPermissions()

--- src/main/java/com/retos/rentacar/modelo/KeyClient.java
+++ src/main/java/com/retos/rentacar/modelo/KeyClient.java
@@ -1,2 +1,54 @@
-package com.retos.rentacar.modelo;public class KeyClient {
+package com.retos.rentacar.modelo;
+
+import com.retos.rentacar.interfaces.ClientInterface;
+import com.retos.rentacar.servicios.GamaServices;
+
+import javax.persistence.Transient;
+import java.util.Optional;
+import java.util.Random;
+
+public class KeyClient {
+
+    @Transient //This column is ignored
+    ClientInterface clientInterface;
+
+
+    private String keyClient;
+
+    public KeyClient() {
+        this.keyClient = generateKeyClient();
+    }
+
+    public String getKeyClient() {
+        return keyClient;
+    }
+
+    public KeyClient(String keyClient) {
+        this.keyClient = keyClient;
+    }
+
+    public String generateKeyClient() {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 12; i++) {
+            int index = random.nextInt(alphabet.length());
+            char randomChar = alphabet.charAt(index);
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
+
+    public Boolean hasPermissions(String key) {
+        Optional<Client> clientToEvaluate = clientInterface.getClientByKeyClient(key);
+        if (clientToEvaluate.isPresent()) {
+            Client client = clientToEvaluate.get();
+            return (client.getType() == ClientType.ADMIN) ||
+                    (client.getType() == ClientType.DEVELOPER);
+        }
+        return false;
+    }
 }
+
+
+
