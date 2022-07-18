631994c1dd1d6c8332e83930cdfd662c3586c429
camlo01 <camilobeltran174@gmail.com>
2022-07-17 22:00:18 -0500

    added class that wrap Car and KeyClient objects for Post request on CarWebRepository

--- src/main/java/com/retos/rentacar/modelo/CarAndKeyclient.java
+++ src/main/java/com/retos/rentacar/modelo/CarAndKeyclient.java
@@ -1,2 +1,27 @@
-package com.retos.rentacar.modelo;public class CarAndKeyclient {
+package com.retos.rentacar.modelo;
+
+public class CarAndKeyclient {
+
+    private Car car;
+    private KeyClient keyClient;
+
+
+    public Car getCar() {
+        return car;
+    }
+
+    public CarAndKeyclient(Car car, KeyClient keyClient) {
+        this.car = car;
+        this.keyClient = keyClient;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public KeyClient getKeyClient() {
+        return keyClient;
+    }
+
+
 }
