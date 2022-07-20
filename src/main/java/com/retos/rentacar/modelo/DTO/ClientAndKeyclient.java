b5996b45db74563a24428b8ea6c5c3657e4b7154
camlo01 <camilobeltran174@gmail.com>
2022-07-20 15:44:42 -0500

    new file added

--- src/main/java/com/retos/rentacar/modelo/DTO/ClientAndKeyclient.java
+++ src/main/java/com/retos/rentacar/modelo/DTO/ClientAndKeyclient.java
@@ -1,2 +1,40 @@
-package com.retos.rentacar.modelo.DTO;public class ClientAndKeyclient {
+package com.retos.rentacar.modelo.DTO;
+
+import com.retos.rentacar.modelo.Entity.Client.Client;
+import com.retos.rentacar.modelo.Entity.Client.KeyClient;
+
+public class ClientAndKeyclient {
+
+    private Client client;
+    private KeyClient keyClient;
+
+    public ClientAndKeyclient(Client client, KeyClient keyClient) {
+        this.client = client;
+        this.keyClient = keyClient;
+    }
+
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public KeyClient getKeyClient() {
+        return keyClient;
+    }
+
+    public void setKeyClient(KeyClient keyClient) {
+        this.keyClient = keyClient;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientAndKeyclient{" +
+                "client=" + client +
+                ", keyClient=" + keyClient +
+                '}';
+    }
 }
