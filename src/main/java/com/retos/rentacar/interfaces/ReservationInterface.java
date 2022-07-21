9a5a273e7575019b5bfb8cc5d934fada6a500306
camlo01 <camilobeltran174@gmail.com>
2022-07-21 15:07:48 -0500

    Deleted Accesibilities of access modifiers of methods

--- src/main/java/com/retos/rentacar/interfaces/ReservationInterface.java
+++ src/main/java/com/retos/rentacar/interfaces/ReservationInterface.java
@@ -5,25 +5,30 @@
 package com.retos.rentacar.interfaces;
 
 import com.retos.rentacar.modelo.Entity.Reservation.Reservation;
+
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import com.retos.rentacar.modelo.Entity.Reservation.ReservationStatus;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
- *
  * @author USUARIO
  */
-public interface ReservationInterface extends CrudRepository<Reservation,Integer> {
-    public List<Reservation> findAllByStatus (String status); 
-    
-    public List<Reservation> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
-    
-    // SELECT clientid, COUNT(*) AS total FROM reservacion group by clientid order by desc;
-    @Query ("SELECT c.client, COUNT(c.client) from Reservation AS c group by c.client order by COUNT(c.client)DESC")
-    public List<Object[]> countTotalReservationsByClient();
+public interface ReservationInterface extends CrudRepository<Reservation, Integer> {
 
-    
-    
+    Optional<Reservation> findReservationByidReservation(int idReservation);
+
+    List<Reservation> findAllByReservationStatus(ReservationStatus status);
+
+    List<Reservation> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+
+    // SELECT clientid, COUNT(*) AS total FROM reservacion group by clientid order by desc;
+    @Query("SELECT c.client, COUNT(c.client) from Reservation AS c group by c.client order by COUNT(c.client)DESC")
+    List<Object[]> countTotalReservationsByClient();
+
+
 }
 
