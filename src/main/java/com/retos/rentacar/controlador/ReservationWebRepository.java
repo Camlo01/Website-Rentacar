a13bdb09e82dab041ac44647c3e93e1bc8fddf45
camlo01 <camilobeltran174@gmail.com>
2022-07-21 15:00:04 -0500

    added different requests for include validation with KeyClient

--- src/main/java/com/retos/rentacar/controlador/ReservationWebRepository.java
+++ src/main/java/com/retos/rentacar/controlador/ReservationWebRepository.java
@@ -1,37 +1,93 @@
 
 package com.retos.rentacar.controlador;
 
+import com.retos.rentacar.modelo.DTO.ReservationAndKeyclient;
 import com.retos.rentacar.modelo.Entity.Reservation.Reservation;
 import com.retos.rentacar.repositorio.CountClients;
 import com.retos.rentacar.servicios.ReservationServices;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/Reservation")
-@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
-        RequestMethod.DELETE })
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+        RequestMethod.DELETE})
 public class ReservationWebRepository {
     @Autowired
     private ReservationServices servicios;
 
 
+    // --- Peticiones HTTP Fijas
+
+    // - GET
+
+    // Client
+    @GetMapping("/my-reservations")
+    public List<Reservation> getMyReservations(@RequestBody Reservation body) {
+        return null;
+    }
+
+
+    //--EMPLOYEES
+
+    // Admin-Developer
+    @GetMapping("/all-reservations")
+    public List<Reservation> getAllReservations() {
+        return null;
+    }
+
+
+    // ONLY FOR TEST
+    @GetMapping("/reservation-by-id")
+    public Optional<Reservation> getReservationById(@RequestBody ReservationAndKeyclient body) {
+        return servicios.getReservationById(body);
+
+    }
+
+    //Support
+    @GetMapping("/reservation-by-client")
+    public Optional<Reservation> getReservationsByClient(@RequestBody Reservation body) {
+
+        return Optional.empty();
+    }
+
+    // reports
+
+
+    // - POST
+
+    // client
+    @PostMapping("/reserve-vehicle")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation reserveVehicle() {
+        return null;
+    }
+
+    // - PUT
+
+    @PutMapping("/edit-reservation")
+    public Reservation editReservation() {
+        return null;
+    }
+
+    @PutMapping("/cancel-reservation")
+    public Reservation cancelReservation() {
+        return null;
+    }
+
+    // - DELETE
+
+
+    // RESOURCES
+
     @GetMapping("/report-dates/{dateOne}/{dateTwo}")
     public List<Reservation> getReservasTiempo(@PathVariable("dateOne") String dateOne,
-            @PathVariable("dateTwo") String dateTwo) {
+                                               @PathVariable("dateTwo") String dateTwo) {
         return servicios.reporteTiempoServicio(dateOne, dateTwo);
     }
 
@@ -41,7 +97,6 @@ public class ReservationWebRepository {
     }
 
 
-    
     @GetMapping("/all")
     public List<Reservation> getReservation() {
         return servicios.getAll();
