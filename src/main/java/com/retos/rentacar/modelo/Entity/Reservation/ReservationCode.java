a74c3cf72237f2d75ad36777f26767384587ee25
camlo01 <camilobeltran174@gmail.com>
2022-07-23 15:54:35 -0500

    Added file that generate code for idetify reservation

---
+++ src/main/java/com/retos/rentacar/modelo/Entity/Reservation/ReservationCode.java
@@ -0,0 +1,37 @@
+package com.retos.rentacar.modelo.Entity.Reservation;
+
+import java.util.Random;
+
+public class ReservationCode {
+
+    private String reservationCode;
+
+    public ReservationCode() {
+        this.reservationCode = generateReservationCode();
+    }
+
+    public ReservationCode(String reservationCode) {
+        this.reservationCode = reservationCode;
+    }
+
+    public String getReservationCode() {
+        return reservationCode;
+    }
+
+    public void setReservationCode(String reservationCode) {
+        this.reservationCode = reservationCode;
+    }
+
+    public String generateReservationCode() {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(alphabet.length());
+            char randomChar = alphabet.charAt(index);
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
+
+}
