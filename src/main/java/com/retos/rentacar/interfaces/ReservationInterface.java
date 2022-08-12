f5b11d7ccf77672c77211c7c2753a4a663f1d135
camlo01 <camilobeltran174@gmail.com>
2022-08-12 15:22:33 -0500

    code documented

--- src/main/java/com/retos/rentacar/interfaces/ReservationInterface.java
+++ src/main/java/com/retos/rentacar/interfaces/ReservationInterface.java
@@ -32,37 +32,87 @@ public interface ReservationInterface extends PagingAndSortingRepository<Reserva
 
     Optional<Reservation> findReservationByCode(String code);
 
-
+    /**
+     * Create a reservation
+     *
+     * @param startDate      start of reservation
+     * @param devolutionDate end of reservation
+     * @param code           code generated automatically
+     * @param car_id         id of car to reserve
+     * @param client_id      id of client who reserve
+     */
     @Modifying
     @Query(value = "INSERT INTO RESERVATION (START_DATE, DEVOLUTION_DATE,CODE, CAR_ID, CLIENT_ID, reservation_status) VALUES (?1, ?2, ?3, ?4, ?5, 'ACTIVE')", nativeQuery = true)
     void createReservation(String startDate, String devolutionDate, String code, int car_id, int client_id);
 
-    //Validate if the car is available for this dates
+
+    /**
+     * Consult
+     *
+     * @param idCar car want to reserve
+     * @param start start Date of the reservation
+     * @param end   end Date of the reservation
+     * @return if exist any reservation, return a List for validate if it's null
+     * @Deprecated
+     */
     @Query(value = "SELECT * FROM reservation WHERE car_id = :carToReserve AND DEVOLUTION_DATE >= :startDate AND START_DATE <= :endDate", nativeQuery = true)
     List<Reservation> isAvailableCarIn(@Param("carToReserve") int idCar,
                                        @Param("startDate") Date start,
                                        @Param("endDate") Date end);
 
+    /**
+     * Reservations actives for the date {reservationsIn}
+     *
+     * @param reservationsIn any Date between any reservation
+     * @return List of active reservations
+     */
     @Query(value = "SELECT * FROM reservation WHERE start_date <= :reservationsIn AND devolution_date >= :reservationsIn AND reservation_status = 'ACTIVE'", nativeQuery = true)
     List<Reservation> getReservationsActiveIn(@Param("reservationsIn") Date reservationsIn);
 
 
+    /**
+     * Reservations made for between these two dates
+     *
+     * @param dateMin minimum date of start the reservation
+     * @param maxDate maximum date of devolution in a reservation
+     * @return List of reservations
+     */
     @Query(value = "SELECT * FROM reservation WHERE start_date >= :minDate AND devolution_date <= :maxDate", nativeQuery = true)
     List<Reservation> getReservationsBetweenDates(@Param("minDate") Date dateMin,
                                                   @Param("maxDate") Date maxDate);
 
-    @Query("SELECT c.client, COUNT(c.client) from Reservation AS c group by c.client order by COUNT(c.client)DESC")
-    List<Object[]> countTotalReservationsByClient();
-
+    /**
+     * Find the client for its key, then and inner join of reservation table and get all reservations
+     *
+     * @param key of client
+     * @return List of reservations
+     */
     @Query(value = "SELECT * FROM CLIENT INNER JOIN RESERVATION ON CLIENT.ID_CLIENT=RESERVATION.ID_CLIENT WHERE CLIENT.KEY_CLIENT LIKE :key", nativeQuery = true)
     List<Reservation> getAllReservationsByClientKey(String key);
 
+    /**
+     * Find the client for its email, then and inner join of reservation table and get all reservations
+     *
+     * @param email of a client
+     * @return List of reservations
+     */
     @Query(value = "SELECT * FROM CLIENT INNER JOIN RESERVATION ON CLIENT.ID_CLIENT=RESERVATION.ID_CLIENT WHERE CLIENT.EMAIL  = ?1", nativeQuery = true)
     List<Reservation> getAllReservationsByClientEmail(String email);
 
+    /**
+     * Find the client for its key, then and inner join of reservation table and get all ACTIVE reservations
+     *
+     * @param key of a client
+     * @return List of active reservations
+     */
     @Query(value = "SELECT * FROM CLIENT INNER JOIN RESERVATION ON CLIENT.ID_CLIENT=RESERVATION.ID_CLIENT WHERE CLIENT.KEY_CLIENT LIKE :key AND RESERVATION_STATUS  = 1", nativeQuery = true)
     List<Reservation> getActiveClientReservation(String key);
 
+
+    //------------------------------ QUERIES UNUSED
+
+    @Query("SELECT c.client, COUNT(c.client) from Reservation AS c group by c.client order by COUNT(c.client)DESC")
+    List<Object[]> countTotalReservationsByClient();
 
 }
 
