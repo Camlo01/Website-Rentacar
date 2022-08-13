8226d1b8be3fff180647705066eda84b5107dfcf
camlo01 <camilobeltran174@gmail.com>
2022-08-13 18:56:41 -0500

    added notification based on the status of request when trying to reserve a vehicle

--- src/main/resources/static/js/loadCars.js
+++ src/main/resources/static/js/loadCars.js
@@ -147,31 +147,83 @@ function reservateCar(id, start, end) {
       .catch((err) => {
         console.log(err);
       });
+
+    reject("Hubo un problema al comunicar con el servidor");
   });
 
   reserveVehicle.then((status) => {
-    console.log("Muy bien");
-    console.log(`status fue ${status.status}`);
-    console.log("se ejecuta el then suelto");
-    if (response.status == 405) {
+    console.log(status);
+    if (status == 405) {
       fetch(`${URL}/reservation/whenCanBeReserveThisVehicle`, {
         method: "POST",
         body: JSON.stringify(data),
         headers: { "Content-type": "application/json; charset=UTF-8" },
       })
-        .then((response2) => {
-          response2.json();
-        })
-        .then((data2) => {
-          console.log(data2.reservation.idReservation);
+        .then((response2) => response2.json())
+        .then(function (data2) {
+          let previousReservation = data2[0];
+          let nextReservation = data2[1];
+
+          let messageWarning = `
+          <div class="alert alert-warning d-flex align-items-center" role="alert">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+              <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+            <div><strong>Hubo un problema!</strong> No puedes reservar este vehículo durante esta fecha</div>
+          </div>
+          `;
+          document.getElementById("whereToPrintResultBeforeReserve").innerHTML =
+            messageWarning;
         });
-    } else if (response.status == 200) {
-      alert("reservado");
+    } else if (status == 201) {
+      let messageSuccess = `
+      <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+  <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+  </symbol>
+  <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+  </symbol>
+  <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+  </symbol>
+</svg>
+      <div class="alert alert-success d-flex align-items-center" role="alert">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+        <div><strong>Felicidades!</strong> Su vehículo fue reservado exitosamente.</div>
+      </div>
+      `;
+      document.getElementById("whereToPrintResultBeforeReserve").innerHTML =
+        messageSuccess;
     } else {
-      alert("AAA");
     }
   });
+
+  reserveVehicle.catch(() => {
+    let messageDanger = `<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+    </symbol>
+    <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+    </symbol>
+    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+    </symbol>
+  </svg>
+  
+  <div class="alert alert-danger d-flex align-items-center" role="alert">
+  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+  <div><stronger>Ups!</stronger> Hubo un problema con el servidor, intenta más tarde o comunicate con el equipo de soporte
+  </div>
+</div> 
+
+  `;
+    document.getElementById("whereToPrintResultBeforeReserve").innerHTML =
+      messageDanger;
+  });
 }
+
 function todaysDate() {
   let todayRaw = new Date();
   todayRaw.toISOString().split("T")[0];
