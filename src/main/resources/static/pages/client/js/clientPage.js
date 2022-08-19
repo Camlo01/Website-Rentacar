0602bd3af2e21203d7844e8fccca36d659911a12
camlo01 <camilobeltran174@gmail.com>
2022-08-18 21:40:09 -0500

    ADDED script file to clientPage

---
+++ src/main/resources/static/pages/client/js/clientPage.js
@@ -0,0 +1,35 @@
+const urlOfApi = `http://localhost:8080/api/rentacar`;
+
+function awd() {
+  let key = {
+    keyClient: "D3V3L0PRR",
+  };
+
+  const history = new Promise(function (resolve, reject) {
+    fetch(
+      `http://localhost:8080/api/rentacar/reservation/my-reservation-history`,
+      {
+        method: "POST",
+        body: JSON.stringify(key),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      }
+    )
+      .then((response) => {
+        console.log(response.status);
+        resolve(response.status);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject("Problema");
+      });
+  });
+  history.then((status) => {
+    console.log("AWD");
+    console.log(status);
+  });
+}
+
+awd();
+// localhost:8080/api/rentacar/test/reservation
+// URLapi
+// http://localhost:8080/api/rentacar/test/reservation
