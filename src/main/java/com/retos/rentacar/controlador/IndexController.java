6d3f0dacc96a3f97ec85c42442a68041807034f8
camlo01 <camilobeltran174@gmail.com>
2022-08-30 14:40:45 -0500

    ADDED index controller by default

---
+++ src/main/java/com/retos/rentacar/controlador/IndexController.java
@@ -0,0 +1,13 @@
+package com.retos.rentacar.controlador;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "Application connected successfully";
+    }
+}
