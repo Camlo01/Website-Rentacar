6a60112bfc112806e87d01bab0d53a5d861d811f
camlo01 <camilobeltran174@gmail.com>
2022-09-01 23:35:01 -0500

    ADDED show sql

---
+++ src/main/java/com/retos/rentacar/modelo/Entity/Car/ImageCar.java
@@ -0,0 +1,10 @@
+package com.retos.rentacar.modelo.Entity.Car;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "images_car")
+public class imagesCar {
+
+}
