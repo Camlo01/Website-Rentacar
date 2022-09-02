61316e0c6fb5c8843043b5f0ca4d8d06e4aa4a11
camlo01 <camilobeltran174@gmail.com>
2022-09-01 23:40:13 -0500

    ADDED car image object

--- src/main/java/com/retos/rentacar/modelo/Entity/Car/ImageCar.java
+++ src/main/java/com/retos/rentacar/modelo/Entity/Car/ImageCar.java
@@ -1,10 +1,55 @@
 package com.retos.rentacar.modelo.Entity.Car;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "images_car")
-public class imagesCar {
+public class ImageCar {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_image", nullable = false)
+    private Integer id;
+
+    @Column(name = "url")
+    private String url;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+    public ImageCar() {
+    }
+    public ImageCar(Integer id, String url, Car car) {
+        this.id = id;
+        this.url = url;
+        this.car = car;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
 }
