5e09e3f6afe55e08717b85ddabb102d156f63f50
camlo01 <camilobeltran174@gmail.com>
2022-11-10 22:36:20 -0500

    separated method to include validation of who make the request

--- src/main/java/com/retos/rentacar/servicios/MessageServices.java
+++ src/main/java/com/retos/rentacar/servicios/MessageServices.java
@@ -1,11 +1,15 @@
 
 package com.retos.rentacar.servicios;
 
+import com.retos.rentacar.modelo.DTO.DAO.MessageDTO;
+import com.retos.rentacar.modelo.Entity.Client.KeyClient;
 import com.retos.rentacar.modelo.Entity.Message.Message;
 import com.retos.rentacar.repositorio.MessageRepository;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,49 +17,56 @@ import org.springframework.stereotype.Service;
 public class MessageServices {
     @Autowired
     private MessageRepository metodosCrudMessage;
+    @Autowired
+    private CarServices carServices;
+    @Autowired
+    private ClientServices clientServices;
 
-    public List<Message> getAll() {
-        return metodosCrudMessage.getAll();
+
+    public List<Message> getMessageOfCar(int id) {
+        return metodosCrudMessage.getAllMessagesOfCar(id);
     }
+
 
     public Optional<Message> getMessage(int idMessage) {
         return metodosCrudMessage.getMessage(idMessage);
     }
 
-    public Message save(Message message) {
-        if (message.getIdMessage() == null) {
-            return metodosCrudMessage.save(message);
-        } else {
-            Optional<Message> evt = metodosCrudMessage.getMessage(message.getIdMessage());
-            if (evt.isEmpty()) {
-                return metodosCrudMessage.save(message);
-            } else {
-                return message;
-            }
+    public Message save(MessageDTO msg) {
+        Message newMessage = new Message();
+        newMessage.setMessageText(msg.getText());
+        newMessage.setCar(carServices.getCar(msg.getIdCar()).get());
+        newMessage.setClient(clientServices.getClientById(msg.getIdClient()).get());
+        return metodosCrudMessage.save(newMessage);
+    }
+
+
+    public Message updateMessage(MessageDTO msg, KeyClient key){
+        if(clientServices.hasPermissions(key, false)){
+            return update(msg);
+        }
+        return null;
+    }
+
+
+    public void deleteMessage(MessageDTO msg, KeyClient key) {
+        if (clientServices.hasPermissions(key, false)) {
+            delete(msg.getId());
         }
     }
 
-    public Message update(Message message) {
-
-        if (message.getIdMessage() != null) {
-            Optional<Message> evt = metodosCrudMessage.getMessage(message.getIdMessage());
-            if (!evt.isEmpty()) {
-                if (message.getMessageText() != null) {
-                    evt.get().setMessageText(message.getMessageText());
-                }
-                metodosCrudMessage.save(evt.get());
-                return message;
-            } else {
-                return message;
-            }
-        } else {
-            return message;
+    //  UTILS
+    //You just can edit the text of the message
+    public Message update(MessageDTO msg) {
+        Message evt = getMessage(msg.getId()).get();
+        if (!Objects.equals(evt.getMessageText(), msg.getText())) {
+            evt.setMessageText(msg.getText());
+            return metodosCrudMessage.save(evt);
         }
-
+        return null;
     }
 
-    public boolean deleteMessage(int idMessage) {
-
+    private boolean delete(int idMessage) {
         Boolean aBoolean = getMessage(idMessage).map(msg -> {
             metodosCrudMessage.delete(msg);
             return true;
