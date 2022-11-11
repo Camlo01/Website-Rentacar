a84fb813e0df973b3d7c5eeb36225ac7dd9625b9
camlo01 <camilobeltran174@gmail.com>
2022-11-10 22:32:25 -0500

    implemented query for return all the messages of a car by its id

--- src/main/java/com/retos/rentacar/interfaces/MessageInterface.java
+++ src/main/java/com/retos/rentacar/interfaces/MessageInterface.java
@@ -5,12 +5,17 @@
 package com.retos.rentacar.interfaces;
 
 import com.retos.rentacar.modelo.Entity.Message.Message;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  *
  * @author USUARIO
  */
 public interface MessageInterface extends CrudRepository<Message,Integer> {
-    
+
+    @Query(value = "SELECT * FROM message WHERE car_id = :idCar",nativeQuery = true)
+    Iterable<Message> getAllMessagesOfCar(@Param("idCar") int idOfCar);
+
 }
