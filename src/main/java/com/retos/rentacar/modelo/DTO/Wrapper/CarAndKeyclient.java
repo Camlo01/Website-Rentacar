98dd3c6c2684e40e09eaaec4d650b4df425b1283
camlo01 <camilobeltran174@gmail.com>
2022-11-12 15:38:03 -0500

    ADDED empty constructor

--- src/main/java/com/retos/rentacar/modelo/DTO/Wrapper/CarAndKeyclient.java
+++ src/main/java/com/retos/rentacar/modelo/DTO/Wrapper/CarAndKeyclient.java
@@ -1,23 +1,28 @@
 package com.retos.rentacar.modelo.DTO.Wrapper;
 
+import com.retos.rentacar.modelo.DTO.DAO.CarDTO;
 import com.retos.rentacar.modelo.Entity.Car.Car;
 import com.retos.rentacar.modelo.Entity.Client.KeyClient;
 
 public class CarAndKeyclient {
 
-    private Car car;
+    private CarDTO car;
     private KeyClient keyClient;
 
-    public CarAndKeyclient(Car car, KeyClient keyClient) {
+
+    public CarAndKeyclient() {
+    }
+
+    public CarAndKeyclient(CarDTO car, KeyClient keyClient) {
         this.car = car;
         this.keyClient = keyClient;
     }
 
-    public Car getCar() {
+    public CarDTO getCar() {
         return car;
     }
 
-    public void setCar(Car car) {
+    public void setCar(CarDTO car) {
         this.car = car;
     }
 
