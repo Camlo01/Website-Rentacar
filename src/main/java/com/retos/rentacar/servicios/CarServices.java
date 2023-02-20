510cd78c77c8de67b7e062479012687aebb32365
unknown <camilobeltran174@gmail.com>
2023-02-19 22:44:17 -0500

    documented code

--- src/main/java/com/retos/rentacar/servicios/CarServices.java
+++ src/main/java/com/retos/rentacar/servicios/CarServices.java
@@ -2,15 +2,12 @@
 package com.retos.rentacar.servicios;
 
 import com.retos.rentacar.modelo.DTO.DAO.CarDTO;
-import com.retos.rentacar.modelo.DTO.DAO.GamaDTO;
 import com.retos.rentacar.modelo.Entity.Car.Car;
-import com.retos.rentacar.modelo.Entity.Car.CarStatus;
 import com.retos.rentacar.modelo.Entity.Client.KeyClient;
 import com.retos.rentacar.modelo.Entity.Gama.Gama;
 import com.retos.rentacar.repositorio.CarRepository;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import com.retos.rentacar.repositorio.GamaRepository;
@@ -19,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CarServices {
+
     @Autowired
     private CarRepository carRepository;
     @Autowired
@@ -26,107 +24,125 @@ public class CarServices {
     @Autowired
     private GamaRepository gamaRepository;
 
-    // GET Methods Without Authorization
+    /**
+     * Method in charge of return a car by its Id
+     *
+     * @return Optional of car
+     */
+    public Optional<Car> getCarById(int idCar) {
+        return carRepository.getCarById(idCar);
+    }
 
+    /**
+     * Method that return all the cars that are in the database
+     *
+     * @return a List of car
+     */
     public List<Car> getAll() {
         return carRepository.getAll();
     }
 
+    /**
+     * Method to get the last car added that is bookable
+     *
+     * @return Optional of car
+     */
     public Optional<Car> getLastCarAddedBookable() {
         return carRepository.getLastCarAddedBookable();
     }
 
-
+    /**
+     * Method to get the last car added no matter its status
+     *
+     * @return Optional of car
+     */
     public Optional<Car> getLastCarAdded() {
         return carRepository.getLastCarAdded();
     }
 
-    public Optional<Car> getCar(int idCar) {
-        return carRepository.getCar(idCar);
-    }
-
-    // POST Method With Authorization
-
-    public Car saveVehicle(CarDTO car, KeyClient keyClient) {
-        if (clientServices.hasPermissions(keyClient, false)) {
-            if(car.getCarStatus() == null){
-                car.setCarStatus(CarStatus.OCCUPIED);
-            }
-            if(car.getGama() == null){
-                Gama emptyGama = gamaRepository.getGama(11).get();
-                car.setGama( new GamaDTO(emptyGama));
-            }
-
-            return carRepository.save(new Car(car));
-        }
-        return null;
-    }
-
-    // PUT Method With Authorization
-
-    public Car updateVehicle(CarDTO car, KeyClient key) {
-        if (clientServices.hasPermissions(key, true)) {
-            return update(new Car(car));
-        }
-        return null;
-    }
-
-    // DELETE Method With Authorization
-
-    // delete vehicle
-    public Boolean deleteVehicle(CarDTO car, KeyClient key) {
+    /**
+     * Method in charge to save a vehicle
+     *
+     * @param car to save
+     * @param key of who request
+     * @return null if the car could not be saved
+     */
+    public Car saveVehicle(Car car, KeyClient key) {
         if (clientServices.hasPermissions(key, false)) {
-            int idOfCarToDelete = car.getId();
+            return carRepository.save(car);
+        }
+        return null;
+    }
 
-            return delete(idOfCarToDelete);
+    /**
+     * Method in charge to update a vehicle
+     *
+     * @param car updated
+     * @param key of who request
+     * @return null if could not be updated
+     */
+    public Car updateVehicle(Car car, KeyClient key) {
+        if (clientServices.hasPermissions(key, true)) {
+            return update(car);
+        }
+        return null;
+    }
+
+    /**
+     * Method in charge to delete a vehicle after verify who request has permissions
+     *
+     * @param car to delete
+     * @param key of who request
+     * @return true if was successfully deleted
+     */
+    public boolean deleteVehicle(CarDTO car, KeyClient key) {
+        Optional<Car> vehicleToDeleteObtained = carRepository.getCarById(car.getId());
+        if (vehicleToDeleteObtained.isPresent()) {
+            if (clientServices.hasPermissions(key, false)) {
+                Car vehicleToDelete = vehicleToDeleteObtained.get();
+                carRepository.delete(vehicleToDelete);
+                return true;
+            }
         }
         return false;
     }
 
     // UTILS METHODS
 
-    private boolean delete(int idCar) {
-        Boolean aBoolean = getCar(idCar).map(car -> {
-            carRepository.delete(car);
-            return true;
-        }).orElse(false);
-        return aBoolean;
-    }
-
+    /**
+     * Method in charge to update a car
+     *
+     * @param car updated
+     * @return null if the car could not be updated
+     */
     private Car update(Car car) {
-        Car evt = carRepository.getCar(car.getId()).get();
-        if (Objects.equals(car.getId(), evt.getId())) {
 
-            if (!Objects.equals(evt.getName(), car.getName())) {
-                evt.setName(car.getName());
-            }
-            if (!Objects.equals(evt.getBrand(), car.getBrand())) {
-                evt.setBrand(car.getBrand());
-            }
-            if (!Objects.equals(evt.getName(), car.getName())) {
-                evt.setName(car.getName());
-            }
-            if (!Objects.equals(evt.getYear(), car.getYear())) {
-                evt.setYear(car.getYear());
-            }
-            if (!Objects.equals(evt.getDescription(), car.getDescription())) {
-                evt.setDescription(car.getDescription());
-            }
-            if (!Objects.equals(evt.getCarStatus(), car.getCarStatus())) {
-                evt.setCarStatus(car.getCarStatus());
-            }
-            if (!Objects.equals(evt.getGama().getId(), car.getGama().getId())){
+        int idOfCarToUpdate = car.getId();
 
-                // Consulting new gama to set
-                Gama gamaToReplace = gamaRepository.getGama(car.getGama().getId()).get();
-                System.out.println(gamaToReplace.toString());
-                evt.setGama(gamaToReplace);
+        Optional<Car> carObtained = carRepository.getCarById(idOfCarToUpdate);
 
+        if (carObtained.isPresent()) {
+            Car carInDB = carObtained.get();
+
+            carInDB.setId(car.getId());
+            carInDB.setName(car.getName());
+            carInDB.setBrand(car.getBrand());
+            carInDB.setYear(car.getYear());
+            carInDB.setDescription(car.getDescription());
+            carInDB.setCarStatus(car.getCarStatus());
+
+            try {
+//                Try to set a new Gama
+                int newGamaId = car.getGama().getId();
+                Gama newGama = gamaRepository.getGamaById(newGamaId).get();
+                carInDB.setGama(newGama);
+
+            } catch (NullPointerException ignored) {
             }
 
-            carRepository.save(evt);
+            return carRepository.save(carInDB);
         }
-        return evt;
+        return null;
     }
 
 
