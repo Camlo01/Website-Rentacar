fd418099d60732dae5eea78d2bbdd85b378d0c17
unknown <camilobeltran174@gmail.com>
2023-02-20 19:55:42 -0500

    Improved code | improved documentation

--- src/main/java/com/retos/rentacar/servicios/ImageCarServices.java
+++ src/main/java/com/retos/rentacar/servicios/ImageCarServices.java
@@ -21,14 +21,33 @@ public class ImageCarServices {
     @Autowired
     private ClientServices clientServices;
 
+    /**
+     * Method in charge of get a image by its id
+     *
+     * @param id of image to find
+     * @return Optional of imageCar
+     */
     public Optional<ImageCar> getImageCarById(int id) {
         return repository.getImageCar(id);
     }
 
+    /**
+     * Method in charge of return a List
+     *
+     * @param idCar of car to get images from
+     * @return List of ImageCar objects
+     */
     public List<ImageCar> getImagesOfCar(int idCar) {
         return repository.getImagesOfCar(idCar);
     }
 
+    /**
+     * Method in charge of save a imageCar
+     *
+     * @param img to save
+     * @param key of who request
+     * @return ImageCar if was successfully saved
+     */
     public ImageCar saveImageCar(ImageCarDTO img, KeyClient key) {
         if (clientServices.hasPermissions(key, false)) {
             ImageCar newImage = new ImageCar();
@@ -39,10 +58,19 @@ public class ImageCarServices {
         return null;
     }
 
-    public void deleteImageCar(ImageCarDTO img, KeyClient key) {
-        if (clientServices.hasPermissions(key, false)){
-            repository.delete(img.getId());
+    /**
+     * Method in charge of delete a imageCar by its id
+     *
+     * @param img to delete
+     * @param key of who request
+     * @return true if was successfully deleted
+     */
+    public boolean deleteImageCar(ImageCarDTO img, KeyClient key) {
+        if (clientServices.hasPermissions(key, false)) {
+            repository.deleteById(img.getId());
+            return true;
         }
+        return false;
     }
 
 
